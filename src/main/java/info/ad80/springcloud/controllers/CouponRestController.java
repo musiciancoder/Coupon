820036e8d73fc862c28a791774563404a9704578
musiciancoder <andresguitarra1980@gmail.com>
2020-09-03 21:37:07 -0400

    79.Test

--- src/main/java/info/ad80/springcloud/controllers/CouponRestController.java
+++ src/main/java/info/ad80/springcloud/controllers/CouponRestController.java
@@ -1,29 +1,36 @@
 package info.ad80.springcloud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import info.ad80.springcloud.model.Coupon;
-import info.ad80.springcloud.model.CouponRepo;
+import info.ad80.springcloud.repos.CouponRepo;
 
 @RestController
-@RequestMapping
+@RequestMapping("/couponapi")
 public class CouponRestController {
 	
 	@Autowired
 	CouponRepo repo;
 	
+	@RequestMapping(value = "/hola", method = RequestMethod.GET)
+	public String HolaMundo() {
+		return "Hola Mundo";
+	}
+	
 	
 	@RequestMapping(value = "/coupons", method = RequestMethod.POST)
-	public Coupon create (Coupon coupon) {
+	public Coupon create (@RequestBody Coupon coupon) {
 		return repo.save(coupon);
 		}
 	
 	
-	@RequestMapping(value = "/coupons/code", method = RequestMethod.GET)
-	public Coupon getCoupon(String code) {
+	@RequestMapping(value = "/coupons/{code}", method = RequestMethod.GET)
+	public Coupon getCoupon(@PathVariable("code") String code) {
 	return repo.findByCode(code);
 		
 	}
--- src/main/java/info/ad80/springcloud/repos/CouponRepo.java
+++ src/main/java/info/ad80/springcloud/repos/CouponRepo.java
@@ -1,6 +1,8 @@
-package info.ad80.springcloud.model;
+package info.ad80.springcloud.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import info.ad80.springcloud.model.Coupon;
 
 public interface CouponRepo extends JpaRepository<Coupon, Long> {
 
